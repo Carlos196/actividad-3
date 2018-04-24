2caff1c288374b542b10d4099b86267e064d0fb5
Carlos196 <36276088+Carlos196@users.noreply.github.com>
2018-04-24 07:05:34 -0500

    Add files via upload
    integrantes: Carlos Andres Peña - Adulis Fuentes - Jose fernando Vertel

---
+++ main.js
@@ -0,0 +1,14 @@
+const vm = new Vue({
+    el: 'main',
+    data:  {
+        capturador: null,
+        AT: 1,
+        NT: 2,
+        Antiguo_testamento: {
+        	descripcion: "genesis, exodo, numeros, deuteronomio"
+        },
+        Nuevo_testamento: {
+        	descripcion: "juan, pedro, mateo, lucas"
+        }
+    },
+});
